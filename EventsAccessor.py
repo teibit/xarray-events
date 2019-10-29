1eb002636df221a819dd6d95451ef0d18c6c6e9e
Ever García <everalfonso.garcia01@estudiant.upf.edu>
2019-10-29 17:33:36 +0100

    Update EventsAccessor.py
    - Added docstrings.
    - Enforced explicit types across arguments and return values.
    - Added missing imports.

--- EventsAccessor.py
+++ EventsAccessor.py
@@ -1,13 +1,44 @@
-import xarray as xr
+import xarray as xr, pandas as pd
+
+from typing import Union
+from pathlib import Path
 
 @xr.register_dataset_accessor('events')
 
 class EventsAccessor:
+    """xarray accessor with extended events-handling functionality.
     
-    def __init__(self, ds):
+    An xarray accessor that extends its functionality to handle events in high-level way. This API makes it easy to load events into an existing Dataset from a variety of sources and perform selections on the events that yield versions of the Datasets satisfying a set of specified constraints.
+    
+    Attributes:
+            _ds (xr.Dataset): The Dataset to be accessed whose class-level functionality is to be extended.
+    
+    """
+    
+    def __init__(self, ds) -> None:
+        """
+        Arguments:
+            ds (xr.Dataset): The Dataset to be accessed whose class-level functionality is to be extended.
+        """
         self._ds = ds
     
-    def load(self, source):
+    def load(self, source: Union[type(pd.DataFrame()), type(Path())]) -> self:
+        """Set the events as an attribute of _ds.
+        
+        Depending on the source where the events are to be found, fetch and load them accordingly.
+        
+        The events will ultimately be represented as a dictionary where the key is the type of event and the value is a DataFrame containing it. Hence, regardless of how they're represented, this method aims to make it fit in this format.
+        
+        Arguments:
+            source (DataFrame/PosixPath): A DataFrame or Path specifying where the events are to be loaded from.
+        
+        Returns:
+            self, which is the modified ds now including events accessed by ds.events from the outside.
+        
+        Usage:
+            First method that should be called on a Dataset upon using this API.
+            
+        """
         
         def split_events(events):
             return {
@@ -15,22 +46,40 @@ class EventsAccessor:
                 for event_type_id, event_df in events.groupby('event_type_id')
             }
         
+        # a DataFrame is the ultimate way of representing the events before splitting them
         if type(source) == type(pd.DataFrame()):
             self._ds = self._ds.assign_attrs(_events = split_events(source))
         
+        # if a Path is given, fetch the data depending on the file extension, convert it into a DataFrame and split the events
         elif type(source) == type(Path()):
             if source.suffix == '.csv':
                 pass
+            pass
         
         else:
             print('Unsupported events source')
         
         return self
     
-    def sel(self, constraints, event_types=None):
+    def sel(self, constraints: dict, event_types: dict = None) -> dict:
+        """Perform a selection on _ds based on a set of constraints in the events.
+        
+        Given a set of constraints specified in a dictionary, select the events that satisfy all of them and then return, for each of them, a new version of _ds. This new _ds will have its dimensions constrained by the specified attributes that literally match the specified constraints.
+        
+        Arguments:
+            constraints (dict): A dictionary that specifies a list of constraints in a dictionary. A key is an attribute from the DataFrame storing the events and a values is a list of potential values.
+            event_types (dict): A dictionary that if specified establishes a correspondance between events and their IDs. A key is the event_type_id and a value is the event name.
+        
+        Returns:
+            datasets (dict): A dictionary that specifies, for each event, the resulting constrained Dataset.
+        
+        Usage:
+            Call strictly after having called load(...) on a Dataset. This ensures that ds.events exists and has the appropriate format that this method expects.
+        
+        """
         
         # self.types is a dict that stores, for each event name, the attributes that we extract from the source
-        self.types = dict({key:list(val.columns) for key,val in zip(self._ds._events,self._ds._events.values())})
+        self.types = dict({k:list(v.columns) for k,v in zip(self._ds._events,self._ds._events.values())})
         
         # self.matching_dims is a dict that filters self.types to only contain values that match dimensions of self._ds literally
         self.matching_dims = {k: list(filter(lambda x: x in self._ds.dims, v)) for k,v in self.types.items()}
