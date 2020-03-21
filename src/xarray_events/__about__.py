969b5bfbffb967d21a2940ec448ab9895a24100a
Ever Garcia <evergarcia@MacBook-MacBook-Pro-de-Ever.local>
2020-03-21 20:14:53 +0100

    Make some final refinements

--- src/xarray_events/__about__.py
+++ src/xarray_events/__about__.py
@@ -1,12 +1,12 @@
 __package_name__ = 'xarray-events'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 __description__ = 'An open-source API for handling events based on xarray.'
 
 __keywords__ = ['numpy', 'pandas', 'xarray', 'events', 'python']
 
 __url__ = 'https://github.com/teibit/xarray-events'
-__download_url__ = 'https://github.com/teibit/xarray-events/archive/v0.1.0.tar.gz'
+__download_url__ = 'https://github.com/teibit/xarray-events/archive/v0.1.1.tar.gz'
 
 __author__ = 'Ever García'
 __author_email__ = 'everalfonso.garcia01@estudiant.upf.edu'
