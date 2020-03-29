ef6d54788fcecd1718f1d424ec89710b36d9e21e
Ever García <everalfonso.garcia01@estudiant.upf.edu>
2020-03-29 22:30:28 +0200

    New version for release

--- src/xarray_events/__about__.py
+++ src/xarray_events/__about__.py
@@ -1,12 +1,12 @@
 __package_name__ = 'xarray-events'
-__version__ = '0.2.1'
+__version__ = '0.2.2'
 
 __description__ = 'An open-source API for handling events based on xarray.'
 
 __keywords__ = ['numpy', 'pandas', 'xarray', 'events', 'python']
 
 __url__ = 'https://github.com/teibit/xarray-events'
-__download_url__ = 'https://github.com/teibit/xarray-events/archive/v0.1.1.tar.gz'
+__download_url__ = 'https://github.com/teibit/xarray-events/archive/v0.2.2.tar.gz'
 
 __author__ = 'Ever García'
 __author_email__ = 'everalfonso.garcia01@estudiant.upf.edu'
